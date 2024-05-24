86d843cb6ec2882fdb8fec6438bf384ff9c45cc8
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-05-24 14:22:33 +0545

    variables completed

--- Day1/variable.php
+++ Day1/variable.php
@@ -5,7 +5,13 @@
 
 $name="Sunil";
 $income=200;
-echo "Name of person $name and income $income"; 
+echo "Name of person $name and income $income<br>"; 
 // Variables are case sensitive in Php
+// Decleration of floating type variable
+$otherinformation=42.6;
+echo "Other information declared $otherinformation<br>";
+// variable cannot start with a number  or must start with letter or an Underscore
+$_neupane="Sunil Neupane";
+echo "underscore variable $_neupane <br>";
 
 ?>
