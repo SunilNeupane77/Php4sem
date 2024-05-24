4be30293cff0b77cd454d0d64278257fd3daf36f
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-05-24 14:07:01 +0545

    first commit

---
+++ Day1/variable.php
@@ -0,0 +1,11 @@
+<?php
+// Vaiable are the containers fo storing information on Php
+// Starts with a $ sign
+
+
+$name="Sunil";
+$income=200;
+echo "Name of person $name and income $income"; 
+// Variables are case sensitive in Php
+
+?>
