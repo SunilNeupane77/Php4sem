de4d238598e4d4569bf00dca0e69bb48eedba30a
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-05-25 14:40:04 +0545

    Math in PHP

---
+++ Day2/math.php
@@ -0,0 +1,28 @@
+<?php
+// The pi() function returns the value of pi()
+echo (pi());
+
+/*
+The php min() and max() functions canbe used to find the lowest or highest value in the list of arguments :
+*/
+echo (min(0,500,-25500));
+echo (max(0,800,89000));
+
+// The abs() function returns the absolute or positive value of a number.
+
+echo (abs((-4.8)));
+
+// The sqrt() functin returns the square root of a number:
+echo (sqrt(100));
+
+// The php round function rounds a floating-point number to its nearest integer
+echo (round((0.89)));
+echo round(0.96);
+
+// The rand() function generates a random number 
+echo (rand());
+// for inclusive (between two numbers)
+echo (rand(1,100));
+
+
+?>
